5db3c990194ee58f2e607b479e7e1b3561b0e547
Amit_Aditaya <amit.aditaya99@gmail.com>
2025-03-08 16:29:28 +0600

    user query fix, final

--- blog/router/blog.py
+++ blog/router/blog.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from .. import schemas
-from ..model import blog_model
+from ..model import blog_model, user_model
 from fastapi import Depends, status, Response, HTTPException
 from sqlalchemy.orm import Session
 from ..database import get_db
@@ -13,16 +13,15 @@ router = APIRouter()
 
 
 @router.post('/blog', tags=['blogs'])
-def create(request : schemas.Blog, db : Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):    
-    user = db.query(blog_model.Blog).filter(blog_model.Blog.user_id == request.user_id).first()
+def create(request: schemas.Blog, db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
+    user = db.query(user_model.User).filter(user_model.User.id == request.user_id).first()
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'user with id {id} not found')    
-    new_blog = blog_model.Blog(title = request.title, body = request.body, user_id= request.user_id)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'user with id {request.user_id} not found')
+    new_blog = blog_model.Blog(title=request.title, body=request.body, user_id=request.user_id)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
-    return new_blog 
-
+    return new_blog
 
 @router.get('/blog',tags=['blogs'])
 def get_all(db : Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
